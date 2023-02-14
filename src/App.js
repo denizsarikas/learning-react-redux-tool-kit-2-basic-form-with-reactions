fa872c522020978686c98e2aae534d15d9236f4a
Deniz Sarıkaş <75947870+denizsarikas@users.noreply.github.com>
2023-02-14 18:30:34 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import PostsList from "./features/posts/PostsList";
+import AddPostForm from "./features/posts/AddPostForm";
+
+function App() {
+  return (
+    <div className="App">
+      <PostsList />
+      <AddPostForm />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/features/posts/PostsList.js
@@ -0,0 +1,38 @@
+import { useSelector } from "react-redux";
+import { selectAllPosts } from "./postsSlice";
+import PostAuthor from "./PostAuthor";
+import TimeAgo from "./TimeAgo";
+import ReactionButtons from "./ReactionButtons";
+
+import React from 'react'
+
+
+const PostsList = () => {
+
+    const posts = useSelector(selectAllPosts) // const posts = useSelector((state) => state.posts)
+
+    const orderedPosts = posts.slice().sort((a, b) => b.date.localeCompare(a.date))
+
+    const renderedPosts = orderedPosts.map(post => (
+        <article key={post.id}>
+            <h3>{post.title}</h3>
+            <p>{post.content.substring(0, 100)}</p>
+
+            <p className="postCredit">
+              <PostAuthor userId={post.userId} />
+              <TimeAgo timestamp={post.date} />
+            </p>
+            <ReactionButtons post={post} />
+        </article>
+    ))
+
+
+  return (
+    <section>
+        <h2>POSTS</h2>
+        {renderedPosts}
+    </section>
+  )
+}
+
+export default PostsList
